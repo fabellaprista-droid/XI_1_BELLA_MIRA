39c20c8c253ffb68ebd002cad37d1f17a794103f
zidan,bella,retno,nadien <fabellaprista@gmail.com>
2026-09-04 13:21:53 +0700

    update project

---
+++ script.js
@@ -0,0 +1,19 @@
+function tampilkanNama() {
+    document.getElementById("namaAnggota").innerHTML =
+    `
+    <ol
+    style="list-style-type: decimal;
+    padding-left:5%;">
+        <li>khaira (khaira@gmail.com)</li>
+        <li>tasya (tasya@gmail.com)</li>
+    </ol>
+
+    <button onclick="location.reload()">
+       tutup kembali
+    </button>   
+
+   `;
+}
+function pesan() {
+    alert("Akan segera hadir");
+}
