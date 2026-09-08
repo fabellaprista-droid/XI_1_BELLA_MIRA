a10289fa1f6951bd925df66877afce9cc1fabb30
zidan,bella,retno,nadien <fabellaprista@gmail.com>
2026-09-08 08:54:17 +0700

    perbaikan

--- script.js
+++ script.js
@@ -4,16 +4,22 @@ function tampilkanNama() {
     <ol
     style="list-style-type: decimal;
     padding-left:5%;">
-        <li>khaira (khaira@gmail.com)</li>
-        <li>tasya (tasya@gmail.com)</li>
+        <li>mira (mira@gmail.com)</li>
+        <li>bella (bella@gmail.com)</li>
     </ol>
 
     <button onclick="location.reload()">
        tutup kembali
-    </button>   
-
+        </button>   
    `;
 }
-function pesan() {
-    alert("Akan segera hadir");
+
+function validasiForm(){
+    var tanggal_mulai = document.getElementById('tgl_mulai')
+    var tanggal_selesai = document.getElementById('tgl_selesai')
+
+    if(new Data (tanggal_selesai) < new Date (tglMulai)){
+        alert('Tanggal Selesai Tidak Boleh Lebih Awal Dari Tanggal Mulai!');
+    }
+        return true;
 }
